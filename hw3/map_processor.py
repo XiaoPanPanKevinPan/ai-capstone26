2f4d9b424a654234e3a3599967476cecec483fec
XiaoPanPanKevinPan <XiaoPanPanKevinPan@users.noreply.github.com>
2026-04-27 10:37:42 +0800

    most are done

--- hw3/map_processor.py
+++ hw3/map_processor.py
@@ -3,8 +3,9 @@ import numpy as np
 from typing import List, Tuple
 
 SCALE_FACTOR = 10000.0 / 255.0
-CEILING_COLOR = np.array([8, 255, 214])
-FLOOR_COLOR = np.array([255, 194, 7])
+# CEILING_COLOR = np.array([8, 255, 214])
+# FLOOR_COLOR = np.array([255, 194, 7])
+MAP_RESOLUTION = 20 # resolution: 5cm per pixel
 
 
 def load_and_filter_map(point_path: str, color_path: str):
@@ -21,7 +22,95 @@ def load_and_filter_map(point_path: str, color_path: str):
     # IMPORTANT: return map_img as float in value range [0, 1] for visualization downstream.
     # NOTE: in habitat sim, x z plane corresponds to world horizontal plane, and y is vertical.
 
-    # return map_img, occupancy_map, ...
+    # 1. remove ceiling and floor by height
+    # - real world is ~2.5m height per floor, 
+    #   but the data contains ~4.0m height due to the floor
+    # - data seems to be collected from a sensor at height 1.5m from the floor
+    #   such that min(data_y) = -1.77m and max(data_y) = 2.365m
+    # - in habitat sim, the agent is a 0.1m-radius pillar, and the sensor height
+    #   is 1.5m from the floor, so we assume the robot has a physical height
+    #   of 1.5 + 0
+    # - so, cut floor < 1.3m and ceiling > 0.0m
+    ceiling_threshold = 0.0
+    floor_threshold = -1.3
+
+    mask_ceiling = coords[:, 1] > ceiling_threshold
+    mask_floor = coords[:, 1] < floor_threshold
+    mask_remove = mask_ceiling | mask_floor
+    
+    coords_cut = coords[~mask_remove]
+    colors_cut = colors[~mask_remove]
+    
+    # 2. project to 2D
+    # - map (x, z) to (u, v)
+    coords_cut_2d = coords_cut[:, [0, 2]]
+    
+    # - The data itself: 
+    #   - min_x = -0.77883, max_x = 0.15992
+    #   - min_z = -0.12620, max_z = 0.25277       
+    min_u = np.min(coords_cut_2d[:, 0])
+    max_u = np.max(coords_cut_2d[:, 0])
+    min_v = np.min(coords_cut_2d[:, 1])
+    max_v = np.max(coords_cut_2d[:, 1])
+    
+    # align top-left to origin
+    coords_cut_2d[:, 0] -= min_u
+    coords_cut_2d[:, 1] -= min_v
+
+    # 3. create map image
+    range_u = max_u - min_u
+    range_v = max_v - min_v
+    map_width = int(range_u * MAP_RESOLUTION) + 1
+    map_height = int(range_v * MAP_RESOLUTION) + 1
+    
+    map_img = np.zeros((map_height, map_width, 3), dtype=np.float32) # pure black background
+    pixel_u = (coords_cut_2d[:, 0] * MAP_RESOLUTION).astype(int)
+    pixel_v = (coords_cut_2d[:, 1] * MAP_RESOLUTION).astype(int)
+    map_img[pixel_v, pixel_u] = colors_cut
+    
+    # 4. remove isolated points
+    occupancy_mask = np.any(map_img > 0, axis=-1).astype(np.uint8)
+    
+    # find connected color blocks and filter out small noise
+    num_labels, labels, stats, centroids = \
+        cv2.connectedComponentsWithStats(
+            occupancy_mask,
+            connectivity=8  # 8: a 3x3 grid centered at the pixel
+                # vs. 4: up/down/left/right neightbours
+        )
+    
+    clean_mask = np.zeros_like(occupancy_mask)
+    for i in range(1, num_labels): # start from 1, skip 0 (background)
+        if stats[i, cv2.CC_STAT_AREA] >= 7: # remove area < 7
+            clean_mask[labels == i] = 1
+
+    # make the background + removed noises white
+    map_img[clean_mask == 0] = 1.0
+
+    # 5. add patches
+    patches = [(159, 87, 160, 100), (91, 36, 93, 47)]
+    for u1, v1, u2, v2 in patches:
+        # draw lines
+        cv2.line(map_img, (u1, v1), (u2, v2), (0, 0, 0), 1)
+        cv2.line(clean_mask, (u1, v1), (u2, v2), 1, 1)
+
+
+    # 6. inflate obstacles to get occupancy map
+    robot_radius_m = 0.20 # the agent is a 0.1m-radius pillar by default, bigger for safety
+    inflate_pixels = int(robot_radius_m * MAP_RESOLUTION)
+    kernel_size = inflate_pixels * 2 + 1
+    kernel_inflate = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (kernel_size, kernel_size))
+    occupancy_map = cv2.dilate(clean_mask, kernel_inflate, iterations=1)
+
+    # extra edition: 
+    # + add a visible inflation
+    # + convert RGB to BGR for cv2
+    map_img_for_selection = map_img.copy()
+    map_img_for_selection[clean_mask == 0] = np.stack([1 - 0.33 * occupancy_map] * 3, axis=-1)[clean_mask == 0]
+    map_img_for_selection = map_img_for_selection[:, :, [2, 1, 0]]
+
+    # return translation constants for TODO4 (pixel to world coordinate)
+    return map_img, occupancy_map.astype(np.float32), map_img_for_selection, (min_u, min_v, MAP_RESOLUTION)
 
 
 def select_start(map_img: np.ndarray) -> Tuple[int, int]:
